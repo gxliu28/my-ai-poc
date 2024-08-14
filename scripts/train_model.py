c82b6cd8c5274fb9dfde81bdf22a55399bc95fc8
gxliu28 <liuganxiang@hotmail.com>
2024-08-14 20:50:31 +0900

    bug fixed

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import argparse
 import torch
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
